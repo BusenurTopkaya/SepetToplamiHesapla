5ba913ca779ada3daff29b461744d27920dbb73c
BusenurTopkaya <busenur998@gmail.com>
2023-12-15 13:03:38 +0300

    SepetToplamiHesapla

---
+++ SepetToplamiHesapla/SepetToplamiHesapla/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace SepetToplamiHesapla
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbUrun = new System.Windows.Forms.ComboBox();
+            this.txtAdet = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblBirimFiyat = new System.Windows.Forms.Label();
+            this.lblToplamFiyat = new System.Windows.Forms.Label();
+            this.lstSepet = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblSepetFiyat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ürün";
+            // 
+            // cmbUrun
+            // 
+            this.cmbUrun.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbUrun.FormattingEnabled = true;
+            this.cmbUrun.Location = new System.Drawing.Point(15, 38);
+            this.cmbUrun.Name = "cmbUrun";
+            this.cmbUrun.Size = new System.Drawing.Size(174, 21);
+            this.cmbUrun.TabIndex = 1;
+            this.cmbUrun.SelectedIndexChanged += new System.EventHandler(this.cmbUrun_SelectedIndexChanged);
+            // 
+            // txtAdet
+            // 
+            this.txtAdet.Location = new System.Drawing.Point(15, 102);
+            this.txtAdet.Name = "txtAdet";
+            this.txtAdet.Size = new System.Drawing.Size(174, 20);
+            this.txtAdet.TabIndex = 2;
+            this.txtAdet.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtAdet_KeyUp);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Adet";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(24, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Birim Fiyat:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(6, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Toplam Fiyat:";
+            // 
+            // lblBirimFiyat
+            // 
+            this.lblBirimFiyat.AutoSize = true;
+            this.lblBirimFiyat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBirimFiyat.Location = new System.Drawing.Point(115, 142);
+            this.lblBirimFiyat.Name = "lblBirimFiyat";
+            this.lblBirimFiyat.Size = new System.Drawing.Size(0, 16);
+            this.lblBirimFiyat.TabIndex = 6;
+            // 
+            // lblToplamFiyat
+            // 
+            this.lblToplamFiyat.AutoSize = true;
+            this.lblToplamFiyat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblToplamFiyat.Location = new System.Drawing.Point(115, 175);
+            this.lblToplamFiyat.Name = "lblToplamFiyat";
+            this.lblToplamFiyat.Size = new System.Drawing.Size(0, 16);
+            this.lblToplamFiyat.TabIndex = 7;
+            // 
+            // lstSepet
+            // 
+            this.lstSepet.FormattingEnabled = true;
+            this.lstSepet.Location = new System.Drawing.Point(205, 12);
+            this.lstSepet.Name = "lstSepet";
+            this.lstSepet.Size = new System.Drawing.Size(263, 212);
+            this.lstSepet.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(278, 237);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Sepet Fiyat:";
+            // 
+            // lblSepetFiyat
+            // 
+            this.lblSepetFiyat.AutoSize = true;
+            this.lblSepetFiyat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSepetFiyat.Location = new System.Drawing.Point(375, 237);
+            this.lblSepetFiyat.Name = "lblSepetFiyat";
+            this.lblSepetFiyat.Size = new System.Drawing.Size(0, 16);
+            this.lblSepetFiyat.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 283);
+            this.Controls.Add(this.lblSepetFiyat);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lstSepet);
+            this.Controls.Add(this.lblToplamFiyat);
+            this.Controls.Add(this.lblBirimFiyat);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAdet);
+            this.Controls.Add(this.cmbUrun);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbUrun;
+        private System.Windows.Forms.TextBox txtAdet;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblBirimFiyat;
+        private System.Windows.Forms.Label lblToplamFiyat;
+        private System.Windows.Forms.ListBox lstSepet;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblSepetFiyat;
+    }
+}
+
---
+++ SepetToplamiHesapla/SepetToplamiHesapla/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SepetToplamiHesapla
+{
+    public partial class Form1 : Form
+    {
+        string[] urunArray = new string[4];
+        decimal[] fiyatArray = new decimal[4];
+        decimal secilenFiyat;
+        string secilenUrun;
+        int quantity;
+        decimal price;
+        List<decimal> toplamSepetFiyat = new List<decimal>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            urunArray[0] = "Çamaşır";
+            fiyatArray[0] = 10;
+            urunArray[1] = "Elbise";
+            fiyatArray[1] = 25;
+            urunArray[2] = "Ayakkabı";
+            fiyatArray[2] = 50;
+            urunArray[3] = "Bilgisayar";
+            fiyatArray[3] = 1500;
+
+            cmbUrun.Items.AddRange(urunArray);
+        }
+
+        private void cmbUrun_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var urunIndex = Array.IndexOf(urunArray, cmbUrun.SelectedItem);
+
+            if (urunIndex != -1)
+            {
+                secilenFiyat = fiyatArray[urunIndex];
+                secilenUrun = urunArray[urunIndex];
+
+                lblBirimFiyat.Text =  secilenFiyat + "₺";
+            }
+        }
+
+        private void txtAdet_KeyUp(object sender, KeyEventArgs e)
+        {
+            //enter tuşuna basıldığı zaman işlem yapmasını istiyorum. e.keyvalue bize tuşların kodunu vermektedir. 
+            if (e.KeyValue == 13) 
+            {
+                bool isNumber = int.TryParse(txtAdet.Text, out int qty);
+
+                if (isNumber)
+                {
+                    quantity = qty;
+                    price = secilenFiyat * quantity;
+                    lblToplamFiyat.Text = string.Format("{0}₺", price);
+
+                    var urun = string.Format("{0} * ({1}) = {2}", secilenUrun, quantity, price);
+                    lstSepet.Items.Add(urun);
+                    toplamSepetFiyat.Add(secilenFiyat * quantity);
+
+                    lblSepetFiyat.Text = toplamSepetFiyat.Sum() + "₺";
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen Doğru Bir Değer Giriniz.");
+                }
+              
+            }
+        }
+    }
+}
